6bbda0ffbec4ab4a56cb61bf98e1cc75bd2bab41
Nishith Modi <nkmodi@ucsc.edu>
2018-10-29 01:55:02 -0700

    youtube embed added

--- discussionboard-app/src/App.js
+++ discussionboard-app/src/App.js
@@ -1,16 +1,20 @@
 import React, { Component } from 'react';
 import './App.css';
 import Header from './components/header';
-import TodoInput from './components/discussionInput'
+import TodoInput from './components/discussionInput';
+import Example from './components/youtubeVideo';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="discussion-wrapper">
-        <Header />
-        <TodoInput />
-        </div>
+			<div className="video-wrapper">
+				<Example />
+			</div>
+			<div className="discussion-wrapper">
+				<Header />
+				<TodoInput />
+			</div>
         
       </div>
     );
